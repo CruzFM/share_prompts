425ef86f67c3b7672432ea72cfc072a7514c12c1
Fer Cruz <cruzfernandomartin94@gmail.com>
2023-05-25 15:12:30 -0300

    providers logic started. to be continued.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,16 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = ()=>{
     const isUserLoggedIn = true;
+
+    const [ providers, setProviders ] = useState(null);
+
+    useEffect( ()=>{
+        const setProviders = async () =>{
+            const response = await getProviders();
+            setProviders(response);
+        }
+        setProviders();
+    })
     return(
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
@@ -21,7 +31,7 @@ const Nav = ()=>{
                 <p className="logo_text">Promptopia</p>
             </Link>
 
-            {/* Mobile Navigation */}
+            {/* Desktop Navigation */}
             <div className="sm:flex hidden">
                 {isUserLoggedIn ? 
                 (<div className="flex gap-3 md:gap-5">
@@ -43,9 +53,24 @@ const Nav = ()=>{
                     </Link>
                 </div>):
                 (<>
-                
+                    {
+                        providers &&
+                        Object.values(providers).map((provider) =>
+                        (
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={()=> signIn(provider.id)}
+                                className="black_btn"
+                            >
+                                Sign In
+                            </button>
+                        ))
+                    }
                 </>)
             }
+
+            {/* Mobile Navigation */}
             </div>
         </nav>
     )
